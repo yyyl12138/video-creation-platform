771efb34ca4df540c459a2744c5f2c2ede2e154a
zjk <84523869@qq.com>
2026-03-17 14:24:48 +0800

    大数据后端接口修改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,15 +14,18 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
+      '/api/offline': {
+        target: 'http://192.168.101.99:5001',
+        changeOrigin: true,
+      },
+      '/api/realtime': {
+        target: 'http://192.168.101.99:5001',
+        changeOrigin: true,
+      },
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path
-      },
-      '/bigdata-api': {
-        target: '192.168.101.99:5001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/bigdata-api/, '')
       },
       '/profile': {
         target: 'http://localhost:8080',
